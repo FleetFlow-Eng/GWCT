db2e507c9a640226a7d10d7b8ab6c0dabd7562cf
Aneto Westfield <anetowestfield@gmail.com>
2026-03-18 17:32:11 -0400

    Finalizing Gwinnett isolation and config

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Added for database
-import { getAuth } from "firebase/auth"; // Added for login
+import { getFirestore } from "firebase/firestore"; 
+import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlobwR52cQZPO-aRQQjMb0XqSlrr1PCZc",
-  authDomain: "bus-inventory.firebaseapp.com",
-  projectId: "bus-inventory",
-  storageBucket: "bus-inventory.firebasestorage.app",
-  messagingSenderId: "449114522566",
-  appId: "1:449114522566:web:465cbef0f2cae216eb28c7",
-  measurementId: "G-SXFCZ0EK9N"
+  apiKey: "AIzaSyDSW4y0G_-PQ-xho4uFlZpAWqfI5pxj2g4",
+  authDomain: "gwct-36433.firebaseapp.com",
+  projectId: "gwct-36433",
+  storageBucket: "gwct-36433.firebasestorage.app",
+  messagingSenderId: "633629068831",
+  appId: "1:633629068831:web:297dc537c2d03fac19cf2a",
+  measurementId: "G-SCWQBD9RJ1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// THESE ARE THE MISSING LINKS:
+// Export the database and authentication instances so the rest of the app can use them
 export const db = getFirestore(app);
 export const auth = getAuth(app);
